243983df22213c84074aad9221b64a28c627a083
devfernandess <109873686+devfernandess@users.noreply.github.com>
2025-04-23 11:12:31 -0300

    conexao.py

--- conexao.py
+++ conexao.py
@@ -3,16 +3,16 @@ from sqlite3 import *
 
 def conectar_banco():
     try:
-        conn= db.connect("sistemaescolar.db")
+        conn= db.connect("banconovo.db")
         return conn, "Conexão bem sucedida!"
     except Error as e:
         return None, f"Error ao conectar: {e}"
     
-def inserir_nome(conn, nome_aluno):
+def inserir_NOME(conn, nome, turma ):
     try:
         cursor = conn.cursor()
-        query = "INSERT INTO aluno (NOME) VALUES(?) "
-        cursor.execute(query, (nome_aluno))
+        query = "INSERT INTO aluno (nome, turma) VALUES(?,?)"
+        cursor.execute(query, (nome, turma,))
         conn.commit()
         return "Nome inserido com sucesso!"
     
